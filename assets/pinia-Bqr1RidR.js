f2c1d026c88d297119afa29fdc0c980ba99180fa
glaures <guido@laures.de>
2024-07-14 17:12:58 +0200

    Updates

---
+++ assets/pinia-Bqr1RidR.js
@@ -0,0 +1,5 @@
+import{f as r,r as l,m as f}from"./@vue-DNa2W_E9.js";var p=!1;/*!
+ * pinia v2.1.7
+ * (c) 2023 Eduardo San Martin Morote
+ * @license MIT
+ */const u=Symbol();var n;(function(t){t.direct="direct",t.patchObject="patch object",t.patchFunction="patch function"})(n||(n={}));function m(){const t=r(!0),i=t.run(()=>l({}));let c=[],s=[];const a=f({install(e){a._a=e,e.provide(u,a),e.config.globalProperties.$pinia=a,s.forEach(o=>c.push(o)),s=[]},use(e){return!this._a&&!p?s.push(e):c.push(e),this},_p:c,_a:null,_e:t,_s:new Map,state:i});return a}export{m as c};
